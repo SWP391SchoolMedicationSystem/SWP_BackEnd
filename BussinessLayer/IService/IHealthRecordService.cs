e0d3cd63849e43dc2431c4793997bdaf21f4756b
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-06-14 23:35:38 +0700

    noti and healthrecord

---
+++ BussinessLayer/IService/IHealthRecordService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.DTO;
+using DataAccessLayer.Entity;
+
+namespace BussinessLayer.IService
+{
+    public interface IHealthRecordService
+    {
+        Task<List<Healthrecord>> GetAllHealthRecordsAsync();
+        Task<HealthRecordDTO> GetHealthRecordByIdAsync(int id);
+        Task AddHealthRecordAsync(HealthRecordDTO healthRecorddto);
+        void UpdateHealthRecord(HealthRecordDTO healthRecorddto);
+        void DeleteHealthRecord(int id);
+        Task<List<Healthrecord>> GetHealthRecordsByStudentIdAsync(int studentId);
+    }
+}
