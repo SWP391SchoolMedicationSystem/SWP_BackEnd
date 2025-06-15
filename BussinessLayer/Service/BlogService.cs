63c205b80d225013a89aba6ba625739e6e805118
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-06-15 20:32:17 +0700

    Them Blog va noti delete
    Them Blog va noti delete

---
+++ BussinessLayer/Service/BlogService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BussinessLayer.IService;
+using BussinessLayer.Utils.Configurations;
+using DataAccessLayer.DTO;
+using DataAccessLayer.Entity;
+using DataAccessLayer.IRepository;
+using DataAccessLayer.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace BussinessLayer.Service
+{
+    public class BlogService : IBlogService
+    {
+        private readonly IBlogRepo _blogRepo;
+        private readonly IMapper _mapper;
+        private readonly AppSetting _appSettings;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        //        private readonly IStaffRepository _staffRepository;
+        public BlogService(
+            IBlogRepo blogRepo,
+            IMapper mapper,
+            IOptionsMonitor<AppSetting> option,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _blogRepo = blogRepo;
+            _mapper = mapper;
+            _appSettings = option.CurrentValue;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<List<Blog>> GetAllBlogsAsync()
+        {
+            List<Blog> blogs = _mapper.Map<List<Blog>>(await _blogRepo.GetAllAsync());
+            return blogs;
+        }
+        public async Task<Blog> GetBlogByIdAsync(int id)
+        {
+            return await _blogRepo.GetByIdAsync(id);
+        }
+        public async Task AddBlogAsync(BlogDTO dto)
+        {
+            if (dto != null)
+            {
+                Blog blog = _mapper.Map<Blog>(dto);
+                await _blogRepo.AddAsync(blog);
+                _blogRepo.Save();
+            }
+        }
+        public void UpdateBlog(BlogDTO dto, int id)
+        {
+            if (dto != null)
+            {
+                var entity = _blogRepo.GetByIdAsync(id).Result;
+                if (entity != null)
+                {
+                    entity.Title = dto.Title;
+                    entity.Content = dto.Content;
+                    entity.ApprovedBy = dto.ApprovedBy;
+                    entity.ApprovedOn = dto.ApprovedOn;
+                    entity.CreatedBy = dto.CreatedBy;
+                    entity.CreatedAt = DateTime.Now;
+                    entity.UpdatedAt = DateTime.Now;
+                    entity.UpdatedBy = dto.UpdatedBy;
+                    entity.Status = dto.Status;
+                    entity.IsDeleted = dto.IsDeleted;
+                    entity.Image = dto.Image;
+
+                    _blogRepo.Update(entity);
+                    _blogRepo.Save();
+                }
+            }
+        }
+        public void DeleteBlog(int id)
+        {
+            var entity = _blogRepo.GetByIdAsync(id).Result;
+            if (entity != null)
+            {
+                _blogRepo.Delete(id);
+                _blogRepo.Save();
+            }
+        }
+        public Task<List<Blog>> SearchBlogsAsync(string searchTerm)
+        {
+            var blogs = _blogRepo.GetAllAsync().Result;
+            return Task.FromResult(blogs.Where(b => b.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                                     b.Content.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).ToList());
+        }
+
+    }
+}
---
+++ SchoolMedicalSystem/Controllers/BlogController.cs
@@ -0,0 +1,66 @@
+﻿using BussinessLayer.IService;
+using BussinessLayer.Service;
+using DataAccessLayer.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolMedicalSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IActionResult> GetAllBlogs()
+        {
+            var blogs = await _blogService.GetAllBlogsAsync();
+            return Ok(blogs);
+        }
+        [HttpGet]
+        [Route("getById/{id}")]
+        public async Task<IActionResult> GetBlogById(int id)
+        {
+            var blog = await _blogService.GetBlogByIdAsync(id);
+            if (blog == null)
+                return NotFound("Blog not found.");
+            return Ok(blog);
+        }
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddBlog([FromBody] BlogDTO blogDto)
+        {
+            if (blogDto == null)
+                return BadRequest("Blog data is null.");
+            await _blogService.AddBlogAsync(blogDto);
+            return Ok("Blog added successfully.");
+        }
+        [HttpPut]
+        [Route("update/{id}")]
+        public IActionResult UpdateBlog([FromBody] BlogDTO dto, int id)
+        {
+            if (dto == null)
+                return BadRequest("Invalid data.");
+
+            _blogService.UpdateBlog(dto, id);
+            return Ok("Health record updated.");
+        }
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public void DeleteBlog(int id)
+        {
+            var blog = _blogService.GetBlogByIdAsync(id).Result;
+            if (blog == null)
+            {
+                throw new KeyNotFoundException($"Blog with id {id} not found.");
+            }
+            _blogService.DeleteBlog(id);
+        }
+    }
+}
