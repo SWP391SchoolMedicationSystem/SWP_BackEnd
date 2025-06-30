c3d9a0f42e3e369b510499c9e9420a1c398e2d12
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-06-30 14:33:53 +0700

    add iservice, repo for studentspecialneed

---
+++ BussinessLayer/IService/IStudentSpecialNeedService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.DTO.StudentSpecialNeeds;
+
+namespace BussinessLayer.IService
+{
+    public interface IStudentSpecialNeedService
+    {
+        Task<List<StudentSpecialNeedDTO>> GetAllStudentSpecialNeedsAsync();
+        Task<StudentSpecialNeedDTO> GetStudentSpecialNeedByIdAsync(int id);
+        void AddStudentSpecialNeed(CreateSpecialStudentNeedDTO studentSpecialNeed);
+        void UpdateStudentSpecialNeed(UpdateStudentSpecialNeedDTO studentSpecialNeed);
+        Task<List<StudentSpecialNeedDTO>> SearchStudentSpecialNeedsAsync(string searchTerm);
+        void DeleteStudentSpecialNeed(int id);
+    }
+}
---
+++ BussinessLayer/Service/StudentSpecialNeedService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessLayer.IService;
+using DataAccessLayer.DTO.StudentSpecialNeeds;
+using DataAccessLayer.Entity;
+using DataAccessLayer.IRepository;
+
+namespace BussinessLayer.Service
+{
+    public class StudentSpecialNeedService : IStudentSpecialNeedService
+    {
+        public void AddStudentSpecialNeed(CreateSpecialStudentNeedDTO studentSpecialNeed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStudentSpecialNeed(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<StudentSpecialNeedDTO>> GetAllStudentSpecialNeedsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StudentSpecialNeedDTO> GetStudentSpecialNeedByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<StudentSpecialNeedDTO>> SearchStudentSpecialNeedsAsync(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStudentSpecialNeed(UpdateStudentSpecialNeedDTO studentSpecialNeed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
