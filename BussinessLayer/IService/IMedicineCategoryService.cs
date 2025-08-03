5fcf5ea283489bcac6f1ca94e66960335fefc85e
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-08-03 22:06:13 +0700

    add medicinecategory

---
+++ BussinessLayer/IService/IMedicineCategoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.DTO.MedicineCategory;
+
+namespace BussinessLayer.IService
+{
+    public interface IMedicineCategoryService
+    {
+        Task<List<MedicineCategoryDTO>> GetAllCategoriesAsync();
+        Task<MedicineCategoryDTO> GetCategoryByIdAsync(int id);
+        Task AddCategory(CreateMedicineCategoryDTO category);
+        Task UpdateCategory(UpdateMedicineCategoryDTO category);
+    }
+}
---
+++ SchoolMedicalSystem/Profiles/MedicineCategoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DataAccessLayer.DTO.MedicineCategory;
+using DataAccessLayer.Entity;
+
+namespace SchoolMedicalSystem.Profiles
+{
+    public class MedicineCategoryProfile : Profile
+    {
+        public MedicineCategoryProfile()
+        {
+            CreateMap<Medicinecategory, MedicineCategoryDTO>()
+                .ReverseMap();
+            CreateMap<CreateMedicineCategoryDTO, Medicinecategory>().ReverseMap();
+            CreateMap<UpdateMedicineCategoryDTO, Medicinecategory>()
+                .ReverseMap();
+        }
+    }
+}
